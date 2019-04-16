7afe873171a6473f5ce62a49d83435509239fd02
cagriyargi <49692049+cagriyargi@users.noreply.github.com>
2019-04-16 20:31:30 +0300

    Create yildizucgen.c

---
+++ yildizucgen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+int main() {
+	
+for(int n = 0; n<5 ; n++ ){
+	    
+	for(int i = 0; n<=i ; i++ ){
+	        
+		printf("*");
+	    
+	}
+	    
+	printf("\n");
+	
+	}
+
+}
